3650c83012bbe32cf4baee3bbc5ba56f372fc25c
Victoria David <victoria.david@escola.pr.gov.br>
2025-07-09 09:26:48 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import{NavigationContainer} 
-from '@react-navigation/native';
-import { createstackNavigator } 
-from '@react-navigation/stack';
+import{NavigationContainer}
+from'@react-navigation/native';
+import{createStackNavigator}
+from'@react-navigation/stack';
 import HomeScreen from './screns/homescreen';
-import SecondaryScreen from './screens/Secondary'
-import CafeScreen from './screen/CafeScreen';
-import BoloScreen from './screen/BoloScreen';
-import PaoScreen from './screen/PaoScreen';
-const Stack= createstackNavigator();
+import SecondaryScreen from './sreeens/Secondary';
+import CafeScreen from './screeens/CafeScreen';
+import BoloScreen from './screeens/BoloScreen';
+import PaoScreen from './screeens/PaoScreen';
+const Stack= createStackNavigator();
 export default function App(){
   return(
-  <NavigationContainer>
-  <Stack.Navigator initialRouteName="Home">
-  <Stack.Screen name="Secondary" component={SecondaryScreen}options={{title: 'Opções Deliciosas'}}/>
-  <Stack.Screen name="Cafe" component={CafeScreen}options={{title: 'Nosso Café'}}/>
-  <Stack.Screen name+"Bolo" component={BoloScreen}options={{title:"Bolo de Chocolate"}}/>
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+    <Stack.Screen name="Home" component= {HomeScreen}options={{title:'Bem Vindo!'}}/
+    <Stack.Screen name="Secondary" component={SecondaryScreen}options={{title:'Opções Deliciosas'}}/>
+    <Stack.Screen name="Cafe" component={CafeScreen}options={{title:'Nosso Café'}}/>
+    <Stack.Screen name="Bolo" component={BoloScreen}options={{title:'Bolo de Chocolate'}}/>
 <Stack.Screen name="Pao" component={PaoScreen}options={{title:'Pao Caseiro'}}/>
 </Stack.Navigator>
 </NavigationContainer>
   );
   }
+
